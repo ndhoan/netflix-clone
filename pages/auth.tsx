447ede98bd6df478c3cddac9dc592c0b81e06ee1
Hoan Nguyen <bosscqt@gmail.com>
2023-05-24 14:42:37 +0700

    first commit

---
+++ pages/auth.tsx
@@ -0,0 +1,131 @@
+import Input from '@/components/input';
+import axios from 'axios';
+import Image from 'next/image';
+import { useCallback, useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
+const Auth = () => {
+  const router = useRouter();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [variant, setVariant] = useState('login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === 'login' ? 'register' : 'login'
+    );
+  }, []);
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/',
+      });
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password,
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
+
+  return (
+    <div
+      className="relative h-full w-full bg-[url('/images/hero.jpg')] 
+    bg-no-repeat bg-fixed bg-cover"
+    >
+      <div className='bg-black w-full h-full lg:bg-opacity-50'>
+        <nav className='px-12 py-5'>
+          <Image
+            className='h-12'
+            src={'/images/logo.png'}
+            alt='Logo'
+            width={100}
+            height={100}
+          />
+        </nav>
+        <div className='flex justify-center'>
+          <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
+            <h2 className='text-white text-4xl mb-8 font-semibold'>
+              {variant === 'login' ? 'Sign In' : 'Register'}
+            </h2>
+            <div className='flex flex-col gap-4'>
+              {variant === 'register' && (
+                <Input
+                  label='Name'
+                  onChange={(event: any) => setName(event.target.value)}
+                  id='name'
+                  type='name'
+                  value={name}
+                />
+              )}
+              <Input
+                label='Email'
+                onChange={(event: any) => setEmail(event.target.value)}
+                id='email'
+                type='email'
+                value={email}
+              />
+              <Input
+                label='Password'
+                onChange={(event: any) => setPassword(event.target.value)}
+                id='password'
+                type='password'
+                value={password}
+              />
+            </div>
+            <button
+              onClick={variant === 'login' ? login : register}
+              className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'
+            >
+              {variant === 'login' ? 'Login' : 'Sign Up'}
+            </button>
+            <div className='flex items-center gap-4 mt-8 justify-center'>
+              <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80  transition'>
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn('github', { callbackUrl: '/' })}
+                className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80  transition'
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
+            <p className='text-neutral-500 mt-12'>
+              {variant === 'login'
+                ? 'First time using Netflix?'
+                : 'Already have an account'}
+              <span
+                onClick={toggleVariant}
+                className='text-white ml-1 hover:underline cursor-pointer'
+              >
+                {variant === 'login' ? 'Create an account' : 'Login'}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
