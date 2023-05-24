ce6b8da9a1079f3c96764d061831b696e7dac641
Hoan Nguyen <bosscqt@gmail.com>
2023-05-24 23:19:44 +0700

    add movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
-import useCurrentUser from '@/hooks/useCurrentUser';
 import Navbar from '@/components/Navbar';
+import BillBoard from '@/components/BillBoard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,11 +23,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
+      <BillBoard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+      </div>
     </>
   );
 }
